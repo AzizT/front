36b5ddff9aa6658fac792acc4f8a9faf50a6a5bf
A.Tobbal <azziz.tobbal@lepoles.com>
2019-01-22 16:51:11 +0100

    22/01

--- JS/js/03_operateurs_arithmetiques_js.js
+++ JS/js/03_operateurs_arithmetiques_js.js
@@ -38,7 +38,25 @@ Modulo de nb1 et nb2 avec l' operateur %
     console.log(resultat);
     document.write(resultat + "<br>");
 
+Reaffectation d' une valeur a nb1:
 
+    nb1 = 11;
+    resultat = nb1 % nb2;
+    console.log(resultat);
+    document.write(resultat);
+    console.log("Le reste de la division de " + nb1 + " par" + nb2 + " est égal a" + resultat);
+    document.write("Le reste de la division de " + nb1 + " par" + nb2 + " est égal a" + resultat);
+
+Les ecritures simplifiées
+
+    nb1 = 15;
+    nb1 = nb1 + 5;
+    console.log(nb1);
+    document.write(nb1);
+
+    nb1 += 10;
+    console.log(nb1);
+    document.write(nb1);
 
 */
 
@@ -66,3 +84,20 @@ var nb1, nb2, resultat;
     resultat = nb1 % nb2;
     console.log(resultat);
     document.write(resultat + "<br>");
+
+    nb1 = 11;
+    resultat = nb1 % nb2;
+    console.log(resultat);
+    document.write(resultat + "<br>");
+
+    console.log("Le reste de la division de " + nb1 + " par " + nb2 + " est égal a " + resultat);
+    document.write("Le reste de la division de " + nb1 + " par " + nb2 + " est égal a " + resultat + "<br>");
+
+    nb1 = 15;
+    nb1 = nb1 + 5;
+    console.log(nb1);
+    document.write(nb1 + "<br>");
+
+    nb1 += 10;
+    console.log(nb1);
+    document.write(nb1);
