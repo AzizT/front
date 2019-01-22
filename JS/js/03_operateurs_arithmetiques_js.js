4c648180f922834c01ffac79606222dde2af8703
A.Tobbal <azziz.tobbal@lepoles.com>
2019-01-22 13:30:03 +0100

    22/01

---
+++ JS/js/03_operateurs_arithmetiques_js.js
@@ -0,0 +1,68 @@
+/*
+*****************************************operateurs arithmetiques***************************
+
+- L' addition
+On peut declarer plusieurs variables a la suite
+
+    var nb1, nb2, resultat;
+    nb1 = 10;
+    nb2 = 5;
+
+addition de nb1 et nb2 avec l' operateur +
+
+    resultat = nb1 + nb2;
+    console;log(resultat);
+    document.write(resultat + "<br>");
+
+soustraction de nb1 et nb2 avec l' operateur -
+
+    resultat = nb1 - nb2;
+    console.log(resultat);
+    document.write(resultat + "<br>");
+
+multiplication de nb1 et nb2 avec l' operateur *
+
+    resultat = nb1 * nb2;
+    console.log(resultat);
+    document.write(resultat + "<br>");
+
+division de nb1 et nb2 avec l' operateur /
+
+    resultat = nb1 / nb2;
+    console.log(resultat);
+    document.write(resultat + "<br>");
+
+Modulo de nb1 et nb2 avec l' operateur %
+
+    resultat = nb1 % nb2;
+    console.log(resultat);
+    document.write(resultat + "<br>");
+
+
+
+*/
+
+var nb1, nb2, resultat;
+    nb1 = 10;
+    nb2 = 5;
+
+
+    resultat = nb1 + nb2;
+    console.log(resultat);
+    document.write(resultat + "<br>");
+
+    resultat = nb1 - nb2;
+    console.log(resultat);
+    document.write(resultat + "<br>");
+
+    resultat = nb1 * nb2;
+    console.log(resultat);
+    document.write(resultat + "<br>");
+
+    resultat = nb1 / nb2;
+    console.log(resultat);
+    document.write(resultat + "<br>");
+
+    resultat = nb1 % nb2;
+    console.log(resultat);
+    document.write(resultat + "<br>");
