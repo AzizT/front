1d05345466a94dd460bb8f17629c46cc71f6eb91
A.Tobbal <45996321+AzizT@users.noreply.github.com>
2019-01-29 21:50:33 +0100

    front

--- JS/exo/js/07_baccalaureat.js
+++ JS/exo/js/07_baccalaureat.js
@@ -1,11 +1,11 @@
-var moyenne;
 var moyenne = prompt("Ecrivez votre moyenne");
 
 if(moyenne<10){
     document.write("Vous etes malheureusement recalé");
-}else if(10<moyenne<12){
+}else if(moyenne>=10 && moyenne<=12){
     document.write("Vous etes reçu");
-}else if(moyenne>12){
-    // document.write("Vous etes reçu avec mention");
-    alert("Vous etes reçu avec mention");
+}else if(moyenne>12 && moyenne<=20){
+    document.write("Vous etes reçu avec mention");
+}else{
+    document.write("Erreur, votre moyenne ne peut exceder 20. Veuillez recommencer !");
 }
