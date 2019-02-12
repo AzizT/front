815feaaf17b1a239210c75d1d79fdddf0934eab6
A.Tobbal <azziz.tobbal@lepoles.com>
2019-02-12 16:44:16 +0100

    12/02

---
+++ JS/exo/js/17_table_multiplication.js
@@ -0,0 +1,3 @@
+chiffre=prompt("Entre un chiffre");
+for(i=1; i<=10; i++) {
+document.write(chiffre+" x "+i+" = "+chiffre*i+"<br><br>");}
---
+++ JS/exo/js/18_oui_non.js
@@ -0,0 +1,6 @@
+var entree=prompt("Ecris quelque chose, sauf; OUI ou NON !");
+while(entree != "oui" || entree != "non"){
+    prompt("continuons, ecris encore");
+}
+document.write("Dommage, c' est perdu");
+
