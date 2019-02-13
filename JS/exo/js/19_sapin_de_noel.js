c0d056dd3c6e59210a9c2e24cd10ca5c20c1923c
A.Tobbal <azziz.tobbal@lepoles.com>
2019-02-13 17:00:18 +0100

    front

--- JS/exo/js/19_sapin_de_noel.js
+++ JS/exo/js/19_sapin_de_noel.js
@@ -9,3 +9,12 @@ for(var etage=1; etage<=branche; etage++){
     }
     
 }
+for(var s=1; s<=branche; s++){
+    document.getElementById("sapin").innerHTML+="||||";
+    document.getElementById("sapin").innerHTML+="<br>";
+}
+/**
+ La proprieté element.innerHTML de l' Element recupere ou définit la syntaxe HTML decrivant les descendants de l' élément
+ La methode getElementById() de Document renvoie un objet Element représentant l' élément dont la propriete id correspond a la chaine de caracteres specifiée. Etant donné que les ID d' element doivent etre uniques
+ S' ils sont spécifiés ils constituent un moyen d' acceder rapidement a un element spécifique
+ */
