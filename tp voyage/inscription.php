30d1e04f35f7a129109b248ea92634e9b2303498
A.Tobbal <45996321+AzizT@users.noreply.github.com>
2019-04-02 06:22:07 +0200

    front

--- tp voyage/inscription.php
+++ tp voyage/inscription.php
@@ -11,6 +11,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="">
 
+    <!-- link pour fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <link href="https://fonts.googleapis.com/css?family=Bangers" rel="stylesheet">
+
     <!-- mon css -->
     <link rel="stylesheet" href="css/style.css">
 
@@ -18,57 +23,83 @@
 
 <body>
 
+<header class="container-fluid">
+
+        <div class="row">
+
+            <!-- <div class="col">
+
+            <nav>
+
+
+            </nav>
+
+        </div> -->
+
+            <nav class="navbar navbar-expand-lg col-md-12 navbar-dark bg-dark">
+                <a class="navbar-brand" href="#"><button type="button" class="btn btn-warning">MENU</button></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+
+                        <a class="nav-item nav-link" href="#"><button type="button" class="btn btn-outline-warning">Reserver</button></a>
+                        <a class="nav-item nav-link" href="#"><button type="button" class="btn btn-outline-warning">Destinations et Escales</button></a>
+                        <a class="nav-item nav-link" href="#"><button type="button" class="btn btn-outline-warning">Les + de TONTON & Fils</button></a>
+                        <a class="nav-item nav-link" href="#"><button type="button" class="btn btn-outline-warning">Aide et Contact</button></a>
+                        <a class="nav-item nav-link" href="#"><button type="button" class="btn btn-outline-warning">Offres d' emplois</button></a>
+                        <a class="nav-item nav-link" href="#"><button type="button" class="btn btn-outline-warning">Mes billets</button></a>
+                    </div>
+                </div>
+            </nav>
+
+        </div>
+
+    </header>
+
     <main>
         <div class="container mt-5">
 
             <form method="post">
-                <div class="col-auto">
-                    <label class="sr-only" for="inlineFormInput">Nom</label>
-                    <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="...">
-                </div>
-
-                <div class="col-auto">
-                    <label class="sr-only" for="inlineFormInput">Prénom</label>
-                    <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="...">
-                </div>
-
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Votre Email</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="...">
-                    <small id="emailHelp" class="form-text text-muted">Soyez rassuré(e), nous ne partagerons aucune de vos données personnelles. Elles resteront confidentielles !</small>
+                <div class="form-group col-md-6">
+                    <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Nom...">
                 </div>
 
                 <div class="form-group col-md-6">
-                    <label for="inputPassword4">Votre Mot de passe</label>
-                    <input type="password" class="form-control" id="inputPassword4" placeholder="...">
+                    <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Prénom...">
                 </div>
 
-                <div class="form-group">
-                    <label for="inputAddress">Votre adresse</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="...">
+                <div class="form-group col-md-6">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Votre Email...">
+                    <small id="emailHelp" class="form-text text-warning">Soyez rassuré(e), nous ne partagerons aucune de vos données personnelles. Elles resteront confidentielles !</small>
                 </div>
 
-                <div class="form-row">
-
-                    <div class="form-group col-md-6">
-                        <label for="inputCity">Ville</label>
-                        <input type="text" class="form-control" id="inputCity">
-                    </div>
-
-                    <div class="form-group col-md-4">
-                        <label for="inputState">Pays</label>
-                        <select id="inputState" class="form-control">
-                            <option selected>Choose...</option>
-                            <option>Algérie</option>
-                            <option>Côte d' Ivoire</option>
-                            <option>Zaïre</option>...</option>
-                            <option>Sénégal</option>
-                            <option>Maroc</option>
-                            <option>Mali</option>
-                        </select>
-                    </div>
-
+                <div class="form-group col-md-4">
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Votre Mot de passe...">
                 </div>
+
+                <div class="form-group col-md-6">
+                    <input type="text" class="form-control" id="inputAddress" placeholder="Votre adresse...">
+                </div>
+
+                <div class="form-group col-md-4">
+                    <input type="text" class="form-control" id="inputCity" placeholder="Votre ville...">
+                </div>
+
+                <div class="form-group col-md-4">
+                    <select id="inputState" class="form-control">
+                        <option selected>Choisissez votre pays...</option>
+                        <option>Algérie</option>
+                        <option>Côte d' Ivoire</option>
+                        <option>Zaïre</option>...</option>
+                        <option>Sénégal</option>
+                        <option>Maroc</option>
+                        <option>Mali</option>
+                    </select>
+                </div>
+
+                <button type="submit" class="offset-5 btn btn-warning">Envoyer</button>
 
             </form>
 
